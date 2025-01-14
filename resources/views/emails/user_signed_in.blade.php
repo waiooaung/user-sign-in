9dc72a8ccfff2f19a60a1519009c19554214407a
waiooaung <waiooaung.sea@gmail.com>
2025-01-15 03:46:00 +0400

    Update user signed in email template with styling and link for user photo

--- resources/views/emails/user_signed_in.blade.php
+++ resources/views/emails/user_signed_in.blade.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>New User Registration</title>
+    <title>User Signed Up</title>
+    <style>
+        @keyframes blink {
+            50% {
+                opacity: 0;
+            }
+        }
+
+        .blink {
+            animation: blink 1s infinite;
+        }
+    </style>
 </head>
 <body>
-    <h1>New User Registration</h1>
+    <h1>User Signed Up</h1>
     <p><strong>Name:</strong> {{ $data['name'] }}</p>
     <p><strong>Date & Time:</strong> {{ $dateTime }}</p>
     @if(isset($data['photo']))
-        <p><strong>Photo:</strong></p>
-        <img src="{{ asset('storage/' . $data['photo']) }}" alt="User Photo" style="max-width: 200px;">
+        <p><strong>Photo:</strong> <a href="{{ asset('storage/' . $data['photo']) }}" class="blink" target="_blank">Click here to view the photo</a></p>
     @endif
 </body>
 </html>
+
